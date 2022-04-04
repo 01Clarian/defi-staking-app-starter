30b959467c9d2c3e7541cb0ca3f4aec87dd81d54
diegomunozme <diego.munoz.me@gmail.com>
2022-04-04 13:33:57 -0700

    wtf is going on

---
+++ defi-staking-app-starter/contracts/Tether.sol
@@ -0,0 +1,9 @@
+pragma solidity >=0.4.22 <0.9.0;
+
+
+contract Tether {
+    string public name = "Mock Tether Coin";
+    string public symbol = "USDT";
+    uint256 public totalSupply = 1000000000000000000;
+    uint8 public decimals = 18; 
+}
