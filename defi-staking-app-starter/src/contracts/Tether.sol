df8870e8500e7bbaeab11608a2a5f76a6e897ed8
diegomunozme <diego.munoz.me@gmail.com>
2022-04-04 13:24:09 -0700

    debugging

--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -2,8 +2,8 @@ pragma solidity ^0.5.0;
 
 
 contract Tether{
-    string public name = 'Tether';
-    string public symbol = 'USDT';
+    string public name = 'Mock Tether Token';
+    string public symbol = 'mUSDT';
     uint256 public totalSupply = 1000000000000000000; // 1 Million Tokens
     uint8 public decimals = 18;
     
