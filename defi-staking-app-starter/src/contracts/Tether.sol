30468ab47cb66a5b35b5c0b5f917ab6f089d13ba
diegomunozme <diego.munoz.me@gmail.com>
2022-04-08 05:39:34 -0700

    deleting files from incorrect directory structure

--- defi-staking-app-starter/src/contracts/Tether.sol
+++ defi-staking-app-starter/src/contracts/Tether.sol
@@ -6,5 +6,32 @@ contract Tether{
     string public symbol = 'mUSDT';
     uint256 public totalSupply = 1000000000000000000; // 1 Million Tokens
     uint8 public decimals = 18;
+
+    event Transfer (
+        address _from,
+        address _to,
+        uint _value
+    );
+    
+    event Approve (
+        address indexed _owner,
+        address indexed _spender,
+        uint _value
+    );
+
+    mapping (address => uint256) public balanceOf;
+
+    constructor() {
+        balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success) {
+        balanceOf[msg.sender] -= _value;
+        balanceOf[_to] += _value;
+        // I need to do a deeper review of the emit keyword
+        emit Transfer(_from, _to, _value); 
+
+        return true;
+    }
     
 }
