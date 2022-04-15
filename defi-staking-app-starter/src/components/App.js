359edd9878f8d61555c08b6817f740bfa4f8d02b
diegomunozme <diego.munoz.me@gmail.com>
2022-04-15 05:13:56 -0700

    Finished loadBlockchainData method

--- defi-staking-app-starter/src/components/App.js
+++ defi-staking-app-starter/src/components/App.js
@@ -2,17 +2,106 @@
 import React, {Component} from "react";
 import './App.css'
 import Navbar from './Navbar.js';
-
+import Web3 from "web3";
+import Tether from '../truffle_abis/Tether.json'
+import RWD from '../truffle_abis/RWD.json'
+import DecentralBank from '../truffle_abis/DecentralBank.json'
 
 class App extends Component {
+
+    async UNSAFE_componentWillMount() {
+        await this.loadWeb3()
+        await this.loadBlockchainData()
+    }
+
+
+    async loadWeb3(){ // This function connects the app to the blockchain, these steps are provided by metamask
+        if (window.ethereum){
+            window.web3 = new Web3(window.ethereum)
+            await window.ethereum.enable()
+        } else if (window.ethereum) {
+                window.web3 = new Web3(window.web3.currentProvider)
+        } else {
+            window.alert('No Eth browser detected, check metamask')
+        }   
+    }
+
+    async loadBlockchainData() {
+        const web3 = window.web3
+        const account = await web3.eth.getAccounts()
+        this.setState({account:account[0]})
+        const newtworkId = await web3.eth.net.getId()
+
+        //Load Tether Contract
+        const tetherData = Tether.networks[newtworkId]
+        if(tetherData){
+            const tether = new web3.eth.Contract(Tether.abi, tetherData.address)
+            this.setState({tether})
+            let tetherBalance = await tether.methods.balanceOf(this.state.account).call()
+            this.setState({tetherBalance: tetherBalance.toString()})
+            console.log({balance:tetherBalance})
+        } else {
+            window.alert('Error! Tether contract not deployed - no detect network!')
+        }
+
+        //Load RWD Data
+        const rwdData = RWD.networks[newtworkId]
+        if(rwdData){
+            const rwd = new web3.eth.Contract(RWD.abi, rwdData.address)
+            this.setState({rwd})
+            let rwdBalance = await rwd.methods.balanceOf(this.state.account).call() 
+            // I need to read web3 docs
+            this.setState({rwdBalance: rwdBalance.toString()})
+            console.log({balance:rwdBalance.toString()})
+        } else {
+            window.alert('Error! Reward Token CONTRACT NOT DEPLOYED')
+        }
+
+        //Load DecentralBank Data
+        const decentralBankData = DecentralBank.networks[newtworkId]
+        if(decentralBankData){
+            const decentralBank = new web3.eth.Contract(DecentralBank.abi, decentralBankData.address)
+            this.setState({decentralBank})
+            let stakingBalance = await decentralBank.methods.stakingBalance(this.state.account).call() 
+            // I need to read web3 docs
+            this.setState({stakingBalance: stakingBalance.toString()})
+            console.log({stakingBalance: stakingBalance.toString()})
+        } else {
+            window.alert('Error! DECENTRAL BANK CONTRACT NOT DEPLOYED')
+        }
+
+        this.setState({loading:false}) 
+        //Change the state of loading data once we've loaded all data
+    } //End Async for loading blockchain data 
+    
+
+    //Props is a special feature in react where we cna pass through special proterties,
+    //In our case we want to begin to integrate metamask with the web application
+    constructor(props){
+        super(props)
+        this.state = {
+            account: '0x0', //we're initializing our state that is changing (the account numbers)
+            tether: {},
+            rwd: {},
+            decentralBank:{},
+            tetherBalance: '0',
+            rwdBalance: '0',
+            stakingBalance: '0',
+            loading: true
+        }
+    }
+
+
+
     //All React code Goes in here
     render() { //renders to the web page
     return ( 
         //Divs are containers that allow us to put html, and css
         <div>
-            <Navbar/>
+            <Navbar account = {this.state.account}/>
             <div className="text-center" > 
-                <h> Hello, World!</h>
+            <h>
+            </h>
             </div>
         </div>
         )
--- defi-staking-app-starter/src/components/Navbar.js
+++ defi-staking-app-starter/src/components/Navbar.js
@@ -1,16 +1,23 @@
 import React, {Component} from "react";
+import bank from '../bank.png'
 
 class Navbar extends Component {
-    //All React code Goes in here
+
     render() { //renders to the web page
     return ( 
         //Nav is the js version of div, but customized specifically for navbars (beneficially)
+        //We're already using bootstrap with these classes
         <nav className="navbar navbar-dark fix-top shadow p-0"
         style={{backgroundColor:'black', height:'50px'}}>
-            <a style={{color:'white'}}> DAPP Yield Farming (Decentralized Banking)</a>
-            <ul>
-                <li>
-                    <small style={{color:'white'}}> Account Number
+            <a className="navbar-brand col-sm-3 col-md-2 mr-0"
+            style={{color:'white'}}> 
+            <img src={bank} width='50' height='30' className='d-inline-block aligh-top'
+            alt='bank'/>
+            &nbsp; DAPP Yield Farming (Decentralized Banking)
+            </a>
+            <ul className="navbar-nav px-3">
+                <li className="text-nowrap d-none nav-item d-sm-none d-sm-block">
+                    <small style={{color:'white'}}> Account Number :{this.props.account}
 
                     </small>
                 </li>
